18eb7a533d4e3294d88af679b48ee44b386084a1
YeisonRojas78 <sonyeirojas@gmail.com>
2025-07-14 18:52:15 -0500

    ajuste cards

--- src/components/CardPago.jsx
+++ src/components/CardPago.jsx
@@ -1,26 +1,11 @@
-// import React from 'react';
 
-// export default function CardPago({ titulo, tipo, pagos, onCrear, onVer }) {
-//   return (
-//     <div className="card">
-//       <h3>{titulo}</h3>
-//       <p>Pagos realizados: {pagos.length}</p>
-//       <button className="btn btn-primary" onClick={() => onCrear(tipo)}>
-//         Crear Pago
-//       </button>
-//       <button className="btn btn-secondary" onClick={() => onVer(tipo)}>
-//         Ver Pagos
-//       </button>
-//     </div>
-//   );
-// }
 export default function CardPago({ titulo, tipo, pagos, onCrear, onVer }) {
   return (
     <div className="card">
       <h3>{titulo}</h3>
       <p>Pagos realizados: {pagos.length}</p>
 
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '12px', flexWrap: 'wrap' }}>
+      <div className="botones">
         <button className="btn-crear" onClick={() => onCrear(tipo)}>Crear Pago</button>
         <button className="btn-ver" onClick={() => onVer(tipo)}>Ver Pagos</button>
       </div>
